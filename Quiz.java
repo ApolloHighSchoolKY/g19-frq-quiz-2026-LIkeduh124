5911e6cd681e14234a28b777da2b35f464805f25
LIkeduh124 <spencer.farley@stu.daviess.kyschools.us>
2026-02-09 20:10:12 +0000

    bleh

--- Quiz.java
+++ Quiz.java
@@ -1,10 +1,38 @@
 public static String scrambleWord(String word){
   /* To be implemented in part (a) */
+  String result = "";
+  for(int pos = 0; pos<word.length(); pos++)
+  {
+    
+    if((word.charAt(pos)=='A')&&(pos != word.length()-1))
+    {
+      result += word.charAt(pos+1);
+      result += 'A';
+      pos++;
+    }
+    else
+    {
+      result += word.charAt(pos);
+    }
+  }
+
+  return result;
 
 }
 
 
 public static void scrambleOrRemove(List<String> wordList){
   /* To be implemented in part (b) */
+  for(int pos = wordList.size()-1; pos>=0; pos--)
+  {
+    if(wordList.get(pos).equals(scrambleWord(wordList.get(pos))))
+    {
+      wordList.remove(pos);
+    }
+    else
+    {
+      wordList.set(pos) = scrambleWord(wordList.get(pos));
+    }
+  }
 
 }
